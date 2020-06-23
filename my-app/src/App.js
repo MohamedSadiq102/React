0792eadd7f32ab63e200ebb9579fe0acc430d9dc
Mohamed Sadiq <mohamed.sadiq@fiware.org>
2020-06-23 22:01:12 +0200

    normal edtion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <h1>My First React App!</h1>
+    <p>Sadiq</p>
+   { /*   <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,9 +18,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to React
         </a>
-      </header>
+      </header> */ }
     </div>
   );
 }
