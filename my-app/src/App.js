94d56578096a3bf7459b5031c27def010873dc28
Mohamed Sadiq <mohamed.sadiq@fiware.org>
2020-06-23 22:25:06 +0200

    Video11 & 12 &Nesting Compenents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Ninjas from './Ninja';
+
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
     <h1>My First React App!</h1>
     <p>Sadiq</p>
+    <Ninjas />
    { /*   <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -23,6 +27,7 @@ function App() {
       </header> */ }
     </div>
   );
+  }
 }
 
 export default App;
